fbb845ae2412a9b9c78c46fb646541dd5af10e10
nughnguyen <hungnq.august@gmail.com>
2026-01-18 17:08:55 +0700

    Update Main.lua with Key System

--- Scripts/Main.lua
+++ Scripts/Main.lua
@@ -1,268 +1,540 @@
-local NughNguyen = loadstring(game:HttpGet('https://raw.githubusercontent.com/nughnguyen/Roblox/refs/heads/main/GUI_VIP.lua'))()
+local GumballZ = loadstring(game:HttpGet("https://raw.githubusercontent.com/nughnguyen/GumballZ-UI-Library/refs/heads/main/GUI/GumballZ-UI-Lib.lua"))();
+local Notification = GumballZ:CreateNotifier();
 
--- Window
-local MainWindow = NughNguyen:CreateWindow({
-   Name = "GumballZ |  Hub",
-   Icon = 89522050759661,
-   LoadingTitle = "Youtube: NughNguyen",
-   LoadingSubtitle = "by QuocHung",
-   Theme = "Ocean",
-
-   DisableRayfieldPrompts = false, 
-   DisableBuildWarnings = false, 
-
-   ConfigurationSaving = {
-      Enabled = false,
-      FolderName = nil,
-      FileName = "GumballZ"
-   },
+GumballZ:Loader({
+	Name = "GumballZ",
+	Duration = 4,
+});
 
 
-
-   Discord = {
-      Enabled = true,
-      Invite = "7r4RzRXzT2",
-      RememberJoins = true
-   },
-
-   KeySystem = true,
-   KeySettings = {
-      Title = "GumballZ | Key System",
-      Subtitle = "Enter your key",
-      Note = "Getkey is very easy, you can also join discord for support!\nDiscord link: https://dsc.gg/thenoicez",
-      FileName = "NughKey",
-      SaveKey = true,
-      GrabKeyFromSite = true,
-      Key = "https://raw.githubusercontent.com/nughnguyen/Roblox/refs/heads/main/key.txt"
-   }
+Notification:Notify({
+	Title = "GumballZ",
+	Content = "Hello, "..game.Players.LocalPlayer.DisplayName..' Welcome back!',
+	Icon = "clipboard"
 })
 
+local Window = GumballZ.new({
+	Name = "GumballZ",
+	Expire = "never",
+	KeySettings = {
+		Title = "GumballZ Key System",
+		Subtitle = "Join Discord to get the key",
+		Note = "Discord Link Copied!",
+		FileName = "GumballZKey",
+		SaveKey = true,
+		GrabKeyFromSite = false,
+		Key = {"1234", "KeyTest"}, -- Example keys, change as needed
+		URL = "https://discord.gg/example"
+	}
+});
 
-
--- Notification
-NughNguyen:Notify({
-   Title = "GumballZ | Hub",
-   Content = "Welcome to GumballZ",
-   Duration = 5,
-   Image = 4483362458,
-   Actions = {
-      Ignore = {
-         Name = "Okay !",
-         Callback = function()
-            print("Ignored")
-         end
-      },
-   },
+local Rage = Window:AddMenu({
+	Name = "RAGE",
+	Icon = "skull"
 })
 
--- Main Tab
-local MainTab = MainWindow:CreateTab("Home", 4034483344)
-
-local Button = MainTab:CreateButton({
-   Name = "Auto Detect & Run Script",
-   Callback = function()
-      local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/nughnguyen/Roblox/refs/heads/main/game_id.lua"))()
-      local placeId = game.PlaceId
-      local scriptUrl = Games[placeId]
-
-      if scriptUrl then
-         NughNguyen:Notify({
-            Title = "GumballZ | Hub",
-            Content = "Đã tìm thấy script, đang tải...",
-            Duration = 2,
-            Image = 4483362458,
-         })
-
-         -- Load script
-         loadstring(game:HttpGet(scriptUrl))()
-
-         NughNguyen:Notify({
-            Title = "GumballZ | Hub",
-            Content = "Script đã được chạy thành công!",
-            Duration = 4,
-            Image = 4483362458,
-         })
-      else
-         NughNguyen:Notify({
-            Title = "Game Not Supported",
-            Content = "Không tìm thấy script hỗ trợ cho game này.",
-            Duration = 5,
-            Image = 4483362458,
-            Actions = {
-               Okay = {
-                  Name = "OK",
-                  Callback = function()
-                     print("Game chưa có script trong GameList.")
-                  end
-               },
-            },
-         })
-      end
-   end,
+local Legit = Window:AddMenu({
+	Name = "LEGIT",
+	Icon = "target"
 })
 
-
-
-
-
-
-
-
-local Toggle = MainTab:CreateToggle({
-   Name = "Anti Afk",
-   Callback = function()
-      -- Chức năng chống afk ở đây
-      local Player = game.Players.LocalPlayer
-      local VirtualUser = game:GetService("VirtualUser")
-      Player.Idled:Connect(function()
-         VirtualUser:Button1Down(Vector2.new(0, 0))
-         wait(1)
-         VirtualUser:Button1Up(Vector2.new(0, 0))
-      end)
-      print("Anti afk đã được kích hoạt")
-   end,
+local Visual = Window:AddMenu({
+	Name = "VISUAL",
+	Icon = "eye"
 })
 
-
--- Các button khác
-
-
-MainTab:CreateButton({
-   Name = "Anime Saga",
-   Callback = function()
-      print('Anime Saga')
-   end,
-}) 
-
-MainTab:CreateButton({
-   Name = "Unload Script",
-   Callback = function()
-      NughNguyen:Notify({
-         Title = "GumballZ | Hub",
-         Content = "Unloading script...",
-         Duration = 2,
-         Image = 4483362458,
-         Actions = {
-            Okay = {
-               Name = "OK",
-               Callback = function()
- 
-               end
-            },
-         },
-      })
-      wait(3)
-      NughNguyen:Destroy()
-      
-   end,
+local Misc = Window:AddMenu({
+	Name = "MISC",
+	Icon = "settings"
 })
 
-
---- Player Tab
-local PlayerTab = MainWindow:CreateTab("Player", 18854794412)
-local slider = PlayerTab:CreateSlider({
-   Name = "WalkSpeed",
-   Range = {16, 100},
-   Increment = 1,
-   Suffix = "WalkSpeed",
-   CurrentValue = 16,
-   Flag = "WalkSpeed",
-   Callback = function(Value)
-      game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
-   end,
-})
-local slider = PlayerTab:CreateSlider({
-   Name = "JumpPower",
-   Range = {50, 200},
-   Increment = 1,
-   Suffix = "JumpPower",
-   CurrentValue = 50,
-   Flag = "JumpPower",
-   Callback = function(Value)
-      game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
-   end,
-})
-local slider = PlayerTab:CreateSlider({
-   Name = "Fov",
-   Range = {70, 120},
-   Increment = 1,
-   Suffix = "Fov",
-   CurrentValue = 70,
-   Flag = "Fov",
-   Callback = function(Value)
-      game.Workspace.CurrentCamera.FieldOfView = Value
-   end,
-})
-local slider = PlayerTab:CreateSlider({
-   Name = "Camera",
-   Range = {0, 100},
-   Increment = 1,
-   Suffix = "Camera",
-   CurrentValue = 0,
-   Flag = "Camera",
-   Callback = function(Value)
-      game.Players.LocalPlayer.CameraMaxZoomDistance = Value
-      game.Players.LocalPlayer.CameraMinZoomDistance = Value
-   end,
-})
-local slider = PlayerTab:CreateSlider({
-   Name = "Gravity",
-   Range = {0, 200},
-   Increment = 1,
-   Suffix = "Gravity",
-   CurrentValue = 0,
-   Flag = "Gravity",
-   Callback = function(Value)
-      game.Workspace.Gravity = Value
-   end,
+local Skins = Window:AddMenu({
+	Name = "SKINS",
+	Icon = "palette"
 })
 
-
--- Shop Tab
-local ShopTab = MainWindow:CreateTab("Sell", 6473525198)
-ShopTab:CreateButton({
-   Name = "Sell All (1 click)",
-   Callback = function()
-   end,
+local Lua = Window:AddMenu({
+	Name = "LUA",
+	Icon = "code"
 })
 
+do
+	local Weapon = Rage:AddSection({
+		Position = 'left',
+		Name = "WEAPON"
+	});
+	
+	local Extra = Rage:AddSection({
+		Position = 'center',
+		Name = "EXTREA"
+	});
+	
+	local General = Rage:AddSection({
+		Position = 'right',
+		Name = "GENERAL"
+	});
+	
+	Weapon:AddSlider({
+		Name = "Hit-chance",
+		Default = 61
+	})
+	
+	Weapon:AddSlider({
+		Name = "Pointscale",
+		Type = "%",
+		Default = 0
+	})
+	
+	Weapon:AddSlider({
+		Name = "Min-damage",
+		Type = "%",
+		Default = 85
+	})
+	
+	Weapon:AddDropdown({
+		Name = "Hitboxes",
+		Values = {"Head",'Neck','Arms','Legs'}
+	})
+	
+	Weapon:AddDropdown({
+		Name = "Multipoint",
+		Values = {"Head",'Neck','Arms','Legs'}
+	})
+	
+	Weapon:AddDropdown({
+		Name = "Target selection",
+		Values = {"Damage"},
+		Default = "Damage"
+	})
+	
+	local Autostop = Extra:AddToggle({
+		Name = "Autostop",
+		Option = true;
+	});
+	
+	Autostop.Option:AddToggle({
+		Name = "Something"
+	})
+	
+	Autostop.Option:AddToggle({
+		Name = "Something"
+	})
+	
+	Autostop.Option:AddToggle({
+		Name = "Something"
+	})
+	
+	Autostop.Option:AddToggle({
+		Name = "Something"
+	})
+	
+	Extra:AddToggle({
+		Name = "Autoscope",
+	});
+	
+	Extra:AddToggle({
+		Name = "Ignore limbs on moving",
+	});
+	
+	Extra:AddToggle({
+		Name = "Autorevolver",
+	});
+	
+	General:AddToggle({
+		Name = "Aimbot",
+		Risky = true
+	})
+	
+	General:AddToggle({
+		Name = "Slient aim",
+		Risky = false
+	})
+	
+	General:AddSlider({
+		Name = "Maximum fov",
+		Type = " deg",
+		Default = 0
+	})
+	
+	General:AddToggle({
+		Name = "Autofire",
+		Risky = false
+	})
+	
+	General:AddToggle({
+		Name = "Delay shot",
+		Risky = false
+	})
+	
+	General:AddToggle({
+		Name = "Duck peek assist",
+		Risky = false
+	})
+	
+	General:AddToggle({
+		Name = "Force bodyaim",
+		Risky = false
+	})
+	
+	General:AddToggle({
+		Name = "Force shoot",
+		Risky = false
+	})
+	
+	General:AddToggle({
+		Name = "Headshot only",
+		Risky = false
+	})
+	
+	General:AddToggle({
+		Name = "Knife bot",
+		Risky = false
+	})
+	
+	General:AddToggle({
+		Name = "Zeus bot",
+		Risky = false
+	})
+	
+	local NoSpread = General:AddToggle({
+		Name = "Nospread",
+		Risky = false,
+		Option = true
+	})
+	
+	NoSpread.Option:AddToggle({
+		Name = "Something"
+	})
 
+	NoSpread.Option:AddToggle({
+		Name = "Something"
+	})
 
+	NoSpread.Option:AddToggle({
+		Name = "Something"
+	})
 
--- Settings Tab
-local SettingsTab = MainWindow:CreateTab("Settings", 9743465390)
+	NoSpread.Option:AddToggle({
+		Name = "Something"
+	})
+	
+	local Doubletap = General:AddToggle({
+		Name = "Doubletap",
+		Risky = true,
+		Option = true
+	})
 
+	Doubletap.Option:AddToggle({
+		Name = "Something"
+	})
 
-SettingsTab:CreateDropdown({
-   Name = "Select Theme",
-   Options = {"Default", "Amber Glow", "Amethyst", "Bloom", "Dark Blue", "Green", "Light", "Ocean", "Serenity"},
-   MultiSelect = false,
-   CurrentOption = "Default",
-   Callback = function()
-     
-   end,
-})
+	Doubletap.Option:AddToggle({
+		Name = "Something"
+	})
 
--- Credits Tab
-local CreditsTab = MainWindow:CreateTab("Credits", 18754976792)
-CreditsTab:CreateLabel("Made by QuocHung")
-CreditsTab:CreateButton({
-   Name = "Join my discord server: https://dsc.gg/thenoicez",
-   Callback = function()
-      setclipboard("https://dsc.gg/thenoicez")
-      NughNguyen:Notify({
-         Title = "GumballZ | Hub",
-         Content = "Đã copy link discord server",
-         Duration = 5,
-         Image = 84828491431270,
-         Actions = {
-            Okay = {
-               Name = "OK",
-               Callback = function()
-                  print("Đã copy link discord server")
-               end
-            },
-         },
-      })
-   end,
-})
+	Doubletap.Option:AddToggle({
+		Name = "Something"
+	})
+
+	Doubletap.Option:AddToggle({
+		Name = "Something"
+	})
+	
+	General:AddButton({
+		Name = "Notification",
+		Callback = function()
+			Notification:Notify({
+				Title = "Notification",
+				Content = "Testing Notification",
+				Duration = math.random(3,7),
+				Icon = "info"
+			})
+		end,
+	})
+end
+
+do
+	local Aim = Legit:AddSection({
+		Position = 'left',
+		Name = "AIM"
+	});
+	
+	local Rcs = Legit:AddSection({
+		Position = 'left',
+		Name = "RCS"
+	});
+
+	local Trigger = Legit:AddSection({
+		Position = 'center',
+		Name = "TRIGGER"
+	});
+	
+	local Backtrack = Legit:AddSection({
+		Position = 'center',
+		Name = "BACKTRACK"
+	});
+
+	local General = Legit:AddSection({
+		Position = 'right',
+		Name = "GENERAL"
+	});
+	
+	Aim:AddToggle({
+		Name = "Aim assist"
+	})
+	
+	Aim:AddDropdown({
+		Name = "Mode",
+		Default = "Adaptive",
+		Values = {"Adaptive","value 1",'Value 2'}
+	})
+	
+	Aim:AddDropdown({
+		Name = "Hitboxes",
+		Multi = true,
+		Default = {
+			["Head"] = true
+		},
+		Values = {
+			"Head",
+			'Neck',
+			'Arms',
+			'Legs'
+		}
+	})
+	
+	Aim:AddSlider({
+		Name = "Multipoint"
+	})
+	
+	Aim:AddSlider({
+		Name = "Aim fov",
+		Round = 1,
+		Default = 0.1,
+		Type = " deg"
+	})
+	
+	Aim:AddSlider({
+		Name = "Aim speed",
+		Default = 1,
+		Type = "%"
+	})
+	
+	Aim:AddSlider({
+		Name = "Min-damage",
+		Default = 61,
+	})
+	
+
+	Aim:AddToggle({
+		Name = "Only in scpoe"
+	})
+	
+
+	Aim:AddToggle({
+		Name = "Autostop"
+	})
+	
+	Rcs:AddToggle({
+		Name = "Recoil control"
+	})
+	
+	Rcs:AddSlider({
+		Name = "Speed",
+		Default = 1,
+		Type = "%"
+	})
+	
+
+	Rcs:AddToggle({
+		Name = "Re-center"
+	})
+	
+
+	Rcs:AddSlider({
+		Name = "Start bullet",
+		Default = 1,
+	})
+	
+	Trigger:AddToggle({
+		Name = "Triggerbot"
+	})
+	
+	Trigger:AddSlider({
+		Name = "Hit-chance",
+		Default = 100,
+		Type = "%"
+	})
+	
+	Trigger:AddToggle({
+		Name = "Use seed when available"
+	})
+	
+	Trigger:AddSlider({
+		Name = "Min-damage",
+		Default = 0,
+		Type = "%"
+	})
+	
+	Trigger:AddSlider({
+		Name = "Reaction time",
+		Default = 0,
+		Type = "ms"
+	})
+	
+	Trigger:AddToggle({
+		Name = "Wait for aim assist hitgroup"
+	})
+	
+	Trigger:AddToggle({
+		Name = "Only in Scope"
+	})
+	
+	Backtrack:AddSlider({
+		Name = "Backtrack",
+		Default = 0,
+		Type = "%"
+	})
+	
+	General:AddToggle({
+		Name = "Enabled"
+	})
+	
+	General:AddDropdown({
+		Name = "Disablers",
+		Values = {"d1",'d2'}
+	})
+	
+	General:AddToggle({
+		Name = "Visualize fov",
+		Option = true
+	}).Option:AddColorPicker({
+		Name = "Color",
+		Default = Color3.fromRGB(255, 34, 75)
+	})
+	
+	General:AddToggle({
+		Name = "Autorevolver"
+	})
+end
+
+do
+	local Misc = Visual:AddSection({
+		Name = "MISC",
+		Position = 'left'
+	})
+	
+	local Model = Visual:AddSection({
+		Name = "MODEL",
+		Position = 'center'
+	})
+	
+	Misc:AddToggle({
+		Name = "Thirdperson",
+		Option = true
+	}).Option:AddSlider({
+		Name = "Distance"
+	});
+	
+	Misc:AddToggle({
+		Name = "Overhead override",
+		Option = true
+	}).Option:AddDropdown({
+		Name = "Override"
+	});
+	
+	Misc:AddToggle({
+		Name = "Fov override",
+		Option = true
+	}).Option:AddToggle({
+		Name = "Something"
+	})
+	
+	Misc:AddToggle({
+		Name = "Viewmodel override",
+		Option = true
+	}).Option:AddToggle({
+		Name = "Something"
+	})
+	
+	Misc:AddDropdown({
+		Name = "Remove scope"
+	})
+	
+	local pc = Misc:AddToggle({
+		Name = "Penetration crosshair",
+		Option = true
+	}).Option;
+	
+	pc:AddColorPicker({
+		Name = "Walls",
+		Default = Color3.fromRGB(111, 255, 0)
+	})
+	
+	pc:AddColorPicker({
+		Name = "Can't walls",
+		Default = Color3.fromRGB(255, 0, 4)
+	})
+	
+	Misc:AddToggle({
+		Name = "Force crosshair",
+		Option = true
+	}).Option:AddToggle({
+		Name = "Something"
+	})
+	
+	Misc:AddDropdown({
+		Name = "Spread"
+	})
+	
+	Misc:AddToggle({
+		Name = "Bullet tracer",
+		Option = true
+	}).Option:AddColorPicker({
+		Name = "Color",
+		Default = Color3.fromRGB(255, 41, 116)
+	})
+	
+	Model:AddDropdown({
+		Name = "Visible",
+		Default = "Disabled",
+		Values = {"Disabled",'Something'}
+	})
+	
+	Model:AddDropdown({
+		Name = "Invisible",
+		Default = "Disabled",
+		Values = {"Disabled",'Something'}
+	})
+	
+	Model:AddDropdown({
+		Name = "Arms",
+		Default = "Disabled",
+		Values = {"Disabled",'Something'}
+	})
+	
+	Model:AddDropdown({
+		Name = "Viewmodel",
+		Default = "Disabled",
+		Values = {"Disabled",'Something'}
+	})
+	
+	Model:AddDropdown({
+		Name = "Attachments",
+		Default = "Disabled",
+		Values = {"Disabled",'Something'}
+	})
+	
+	Model:AddToggle({
+		Name = 'Glow',
+		Option = true
+		
+	}).Option:AddColorPicker({
+		Name = "Color"
+	})
+	
+	Model:AddKeybind({
+		Name = "Toggle"
+	})
+
+end
